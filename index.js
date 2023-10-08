33a5525b61136f8f037ecb1501bc3966e2584570
Jull128 <88882567+Jull128@users.noreply.github.com>
2023-10-09 01:48:37 +0500

    play

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+// import { WaveSurfer } from "wavesurfer.js";
+
+// Экземпляр плеера
+// window.onload = function () {
+const player = WaveSurfer.create({
+  container: ".waveform",
+  waveColor: "grey",
+  progressColor: "blue",
+  cursorColor: "red",
+  barHeight: 2,
+  height: 50,
+});
+
 const play = document.querySelector(".play-pause");
 const mix = document.querySelector(".mix");
 const back = document.querySelector(".back");
@@ -7,3 +20,47 @@ const seekBar = document.querySelector(".seek-bar");
 const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".current-time");
 const duration = document.querySelector(".duration");
+const playlist = document.querySelector(".playlist");
+
+const arrayPlaylist = [
+  "audio/96_minutes.mp3",
+  "audio/batarei.mp3",
+  "audio/glupaya.mp3",
+];
+
+const currentIndex = 0;
+
+arrayPlaylist.forEach((audio, id) => {
+  const track = document.createElement("li");
+  track.textContent = `${id + 1}`;
+  track.addEventListener("click", () => {
+    currentIndex = id;
+    playTrack(currentIndex);
+  });
+  playlist.appendChild(track);
+});
+
+const playTrack = (id) => {
+  const trackToPlay = arrayPlaylist[id];
+  player.load(trackToPlay);
+  console.log(trackToPlay);
+  player.on("ready", () => {
+    player.play();
+    //поменять иконку на паузу
+    // функция изменения времени
+  });
+};
+
+play.addEventListener("click", () => {
+  //   playTrack(currentIndex);
+  if (player.isPlaying()) {
+    player.pause();
+  } else {
+    player.play();
+  }
+});
+
+// Загрузка первого трека при загрузке страницы
+
+playTrack(currentIndex);
+// };
